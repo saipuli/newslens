c85dd0ac7c82dc50e45caf2a8e060e674958c431
Sai Puli <sai.puli@getbeyond.com>
2020-12-17 00:30:08 -0600

    tweaked

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
-  let url = 'http://feeds.bbci.co.uk/news/technology/rss.xml';
-  const textarea = document.querySelector('#feed-textarea > ul');
+let url = "https://www.indiatoday.in/rss/1206578";
+const textarea = document.querySelector("#feed-textarea");
 
-  const date = new Date();
-  document.querySelector('#date').innerHTML = date.toDateString();
-    
-    feednami.load(url)
-    .then(feed => {
-      textarea.value = ''
-      for(let entry of feed.entries){
-          //create a list element
-          let li = document.createElement('li');
-          //add HTML content to list items
-          li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
-          //append HTML content to list 
-          textarea.appendChild(li);
-      }
-    });
+const date = new Date();
+document.querySelector("#date").innerHTML = date.toDateString();
 
+feednami.load(url).then((feed) => {
+  textarea.value = "";
+  // console.log(feed.entries);
+  for (let entry of feed.entries.slice(feed.entries, 12)) {
+    //create a list element
+    let li = document.createElement("div");
+    li.className = "min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800";
+    let stripper = document.createElement("div");
+    stripper.innerHTML = entry.description;
+    let textContent = stripper.textContent || stripper.innerText || "";
+
+    if (entry.title.toUpperCase() === textContent.trim().toUpperCase()) {
+      console.log("title and description same...");
+      textContent = "";
+    } else {
+      console.log("they're different");
+    }
+
+    var firstImage = stripper.getElementsByTagName("img")[0];
+    var imgSrc = firstImage ? firstImage.src : "";
+    if(imgSrc)
+    {
+      firstImage = document.createElement('img');
+      firstImage.src = imgSrc;
+      firstImage.className = "h-8 w-8 rounded-full object-cover mx-1";
+    }
+
+    const cardHtml = `
+               <h4 class="mb-4 font-semibold text-gray-600 dark:text-gray-300">
+               <a href="${entry.link}">${entry.title}</a>
+               </h4>
+               ${firstImage.outerHTML}
+               <p class="text-gray-600 dark:text-gray-400">
+                ${textContent}
+               </p>`;
+
+    //add HTML content to list items
+    // li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4><p>${entry.description}</p>`;
+    li.innerHTML = cardHtml;
+    //append HTML content to list
+    textarea.appendChild(li);
+  }
+});
 
 //Using feednami to fetch RSS feeds
 //https://toolkit.sekando.com/docs/en/feednami
 
 //Feeds from BBC News
 //https://www.bbc.com/news/10628494#userss
-  
